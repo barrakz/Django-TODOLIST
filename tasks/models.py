60be4ecb34559d8ded13841a1d7257eb4d6ea82e
barrakz <b.rakuzy@gmail.com>
2023-02-06 21:42:03 +0100

    login register auth test

--- tasks/models.py
+++ tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -18,6 +19,7 @@ class Task(models.Model):
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from .models import Task, Category
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 # MAIN MENU AND TASKS ADD AND EDIT
-
+@login_required
 def index(request):
     print(request.GET)
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks = Task.objects.filter(user=request.user).order_by('-created_at')
     categories = Category.objects.all()
     status = request.GET.get('status')
     if status == "True":
@@ -19,17 +22,19 @@ def index(request):
     return render(request, 'tasks/index.html', context)
 
 
+@login_required
 def add(request):
     categories = Category.objects.all()
     if request.method == 'POST':
         name = request.POST['name']
         category = Category.objects.get(id=request.POST['category'])
-        Task.objects.create(name=name, category=category)
+        Task.objects.create(name=name, category=category, user=request.user)
         return redirect('index')
     context = {'categories': categories}
     return render(request, 'tasks/add.html', context)
 
 
+@login_required
 def edit(request, pk):
     task = Task.objects.get(id=pk)
     if request.method == 'POST':
@@ -40,11 +45,13 @@ def edit(request, pk):
     return render(request, 'tasks/edit.html', context)
 
 
+@login_required
 def delete(request, pk):
     Task.objects.get(id=pk).delete()
     return redirect('index')
 
 
+@login_required
 def complete_task(request, task_id):
     task = Task.objects.get(pk=task_id)
     task.completed = request.POST.get('completed', False) == 'on'
@@ -53,7 +60,7 @@ def complete_task(request, task_id):
 
 
 # CATEGORIES
-
+@login_required
 def add_category(request):
     categories = Category.objects.all()
     if request.method == 'POST':
@@ -64,6 +71,7 @@ def add_category(request):
     return render(request, 'tasks/add_category.html', context)
 
 
+@login_required
 def edit_category(request, pk):
     category = Category.objects.get(id=pk)
     if request.method == 'POST':
@@ -74,6 +82,7 @@ def edit_category(request, pk):
     return render(request, 'tasks/edit_category.html', context)
 
 
+@login_required
 def delete_category(request, pk):
     try:
         default_category = Category.objects.get(name='None')
@@ -87,3 +96,28 @@ def delete_category(request, pk):
         task.save()
     Category.objects.get(id=pk).delete()
     return redirect('add_category')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log the user in
+            return redirect('index')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
